4b442c6ed23cc12901fe6395d62bb78616398e3b
Shince12 <shincejose12@gmail.com>
2022-12-30 20:17:49 +0530

    first commit

---
+++ products.js
@@ -0,0 +1,36 @@
+export const products = [
+
+    {
+        id: 1,
+        name: 'Jungle Book',
+        price: 100,
+        image:require('./assets/jungle.jpg')
+
+    },
+    {
+        id: 2,
+        name: 'Aladdin',
+        price: 95,
+        image:require('./assets/aladdin.jpg')
+    },
+    {
+        id: 3,
+        name: 'My Experiment with Truth',
+        price: 200,
+        image:require('./assets/gandhi.jpg')
+    },
+    {
+        id: 4,
+        name: 'Call Of Wild',
+        price: 180,
+        image:require('./assets/call.jpg')
+ 
+    },
+    {
+        id: 5,
+        name: 'Dairy Of Wimby Kid',
+        price: 300,
+        image:require('./assets/dairy.jpg')
+    },
+
+]
